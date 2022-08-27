8b637898c47e4c9fb67328abfab2adcfa29d0340
stevenAnto <ecalcinap@unsa.edu.pe>
2022-08-27 14:52:27 -0500

    se pone una pila, para que ir sacando los vertices visitados y se pone un contador cada vez que vuelve entrar

--- src/main/java/com/example/project/GraphMatrix.java
+++ src/main/java/com/example/project/GraphMatrix.java
@@ -4,97 +4,110 @@ import java.util.ArrayList;
 
 public class GraphMatrix implements Graph {
 
-    private int numVertices;
-    private int[][] adjacency;
+  private int numVertices;
+  private int[][] adjacency;
+  private LinkedListStack<Integer> pilaEnteros;
 
-    public GraphMatrix(int numVertices) {
-        this.numVertices = numVertices;
-        this.adjacency = new int[numVertices][numVertices];
+  public GraphMatrix(int numVertices) {
+    this.numVertices = numVertices;
+    this.adjacency = new int[numVertices][numVertices];
+    llenarStack(numVertices);
 
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                this.adjacency[i][j] = 0;
-            }
-        }
+    for (int i = 0; i < numVertices; i++) {
+      for (int j = 0; j < numVertices; j++) {
+	this.adjacency[i][j] = 0;
+      }
     }
-
-    @Override
-    public boolean addEdge(int from, int to) {
-        if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
-            this.adjacency[from][to] = 1;
-            this.adjacency[to][from] = 1;
-            return true;
-        }
-        return false;
+  }
+  public void llenarStack(int numVertices){
+    pilaEnteros = new LinkedListStack<Integer>();
+    for(int i=0; i<numVertices;i++){
+      pilaEnteros.push(i);
     }
+  }
 
-    @Override
-    public boolean removeEdge(int from, int to) {
-        if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
-            this.adjacency[from][to] = 0;
-            this.adjacency[to][from] = 0;
-            return true;
-        }
-        return false;
+  @Override
+  public boolean addEdge(int from, int to) {
+    if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+      this.adjacency[from][to] = 1;
+      this.adjacency[to][from] = 1;
+      return true;
     }
+    return false;
+  }
 
-    public boolean vertexDoesExist(int aVertex) {
-        if (aVertex >= 0 && aVertex < this.numVertices) {
-            return true;
-        } else {
-            return false;
-        }
+  @Override
+  public boolean removeEdge(int from, int to) {
+    if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+      this.adjacency[from][to] = 0;
+      this.adjacency[to][from] = 0;
+      return true;
     }
+    return false;
+  }
 
-    public ArrayList<Integer> depthFirstSearch(int n) {
-        return this.depthFirstSearch(n, new ArrayList<Integer>());
+  public boolean vertexDoesExist(int aVertex) {
+    if (aVertex >= 0 && aVertex < this.numVertices) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public ArrayList<Integer> depthFirstSearch(int n, ArrayList<Integer> visited) {
-        visited.add(n);
-        for (int i = 0; i < this.numVertices; i++) {
-            if (this.adjacency[n][i] == 1 && !visited.contains(i)) {
-                depthFirstSearch(i, visited);
-            }
-        }
-        return visited;
+  public ArrayList<Integer> depthFirstSearch(int n) {
+    return this.depthFirstSearch(n, new ArrayList<Integer>());
+  }
+
+  public ArrayList<Integer> depthFirstSearch(int n, ArrayList<Integer> visited) {
+    visited.add(n);
+    pilaEnteros.pop();
+    for (int i = 0; i < this.numVertices; i++) {
+      if (this.adjacency[n][i] == 1 && !visited.contains(i)) {
+	depthFirstSearch(i, visited);
+      }
     }
+    return visited;
+  }
 
-    public String toString() {
-        String s = "    ";
-        for (int i = 0; i < this.numVertices; i++) {
-            s = s + String.valueOf(i) + " ";
-        }
-        s = s + " \n";
-
-        for (int i = 0; i < this.numVertices; i++) {
-            s = s + String.valueOf(i) + " : ";
-            for (int j = 0; j < this.numVertices; j++) {
-                s = s + String.valueOf(this.adjacency[i][j]) + " ";
-            }
-            s = s + "\n";
-        }
-        return s;
+  public String toString() {
+    String s = "    ";
+    for (int i = 0; i < this.numVertices; i++) {
+      s = s + String.valueOf(i) + " ";
     }
+    s = s + " \n";
 
-    public int countConnectedComponents() {
-
-        return -1;
+    for (int i = 0; i < this.numVertices; i++) {
+      s = s + String.valueOf(i) + " : ";
+      for (int j = 0; j < this.numVertices; j++) {
+	s = s + String.valueOf(this.adjacency[i][j]) + " ";
+      }
+      s = s + "\n";
     }
+    return s;
+  }
 
-    public static void main(String args[]) {
-        GraphMatrix graph = new GraphMatrix(5);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 3);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 4);
-        System.out.println("The graph matrix:");
-        System.out.println(graph);
-        System.out.println("DFS:");
-        System.out.println(graph.depthFirstSearch(0));
+  public int countConnectedComponents() {
+    int contador=0;
+    while(!pilaEnteros.isEmpty()){
+      depthFirstSearch(pilaEnteros.top());
+      contador++;
     }
+    return contador;
+  }
+
+  public static void main(String args[]) {
+    GraphMatrix graph = new GraphMatrix(5);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(0, 3);
+    graph.addEdge(0, 4);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 4);
+    System.out.println("The graph matrix:");
+    System.out.println(graph);
+    System.out.println("DFS:");
+    System.out.println(graph.depthFirstSearch(0));
+  }
 
 }
